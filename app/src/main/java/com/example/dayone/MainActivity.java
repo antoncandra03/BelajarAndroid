ea39ad788225a94b8274142442fafe0007eac47e
antoncandra03 <anton.candra03@gmail.com>
2019-04-08 17:08:33 +0700

    update mainActivity

--- app/src/main/java/com/example/dayone/MainActivity.java
+++ app/src/main/java/com/example/dayone/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.dayone;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.dayone.model.Orang;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int REQUEST_CODE = 1;
 
     // TODO 1.1 deklarasi widget/button yg digunakan
     Button btnMove, btnPassData, btnPassObj,
@@ -61,17 +66,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.btn_callback:
-//                callBack();
-                break;
-
-            case R.id.btn_email:
-//                email();
+                callBack();
                 break;
 
             case R.id.btn_call:
-//                call();
+                implicitCall();
                 break;
 
+            case R.id.btn_email:
+                implicitEmail();
+                break;
+
+
+        }
+    }
+
+    // TODO 6.1
+    private void implicitEmail() {
+        Intent email = new Intent(Intent.ACTION_SENDTO,
+                Uri.fromParts("mailto", "anton.candra03@gmail.com", null));
+
+        email.putExtra(Intent.EXTRA_SUBJECT, "Ini Subject");
+        email.putExtra(Intent.EXTRA_TEXT, "Ini Body Message");
+
+        if (email.resolveActivity(getPackageManager())!= null){
+            startActivity(Intent.createChooser(email, "Pilih share client"));
+        }else {
+            Toast.makeText(this, "Tidak ada share client", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    // TODO 5.1 intent implicit call
+    private void implicitCall() {
+        Intent call = new Intent(Intent.ACTION_DIAL);
+        call.setData(Uri.parse("tel:08561668081"));
+        startActivity(call);
+    }
+
+    // TODO 4.6
+    private void callBack() {
+        Intent callback = new Intent(MainActivity.this, Callback.class);
+
+        startActivityForResult(callback, REQUEST_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // TODO 4.7 buat kondisi kalo seandainya request codenya sama dan data nya ada
+        if (requestCode == REQUEST_CODE){
+            if (resultCode == RESULT_OK){
+                String temp = data.getStringExtra(Callback.EXTRA_DATA);
+                Toast.makeText(this, temp, Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
